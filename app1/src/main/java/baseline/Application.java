70604b17a3c94f2c116511627b247c192ec0550e
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-24 11:11:06 -0400

    attempted to delete files but it failed for unknown reasons

--- app1/src/main/java/baseline/Application.java
+++ app1/src/main/java/baseline/Application.java
@@ -1,10 +1,13 @@
 package pack1;
 
+import javafx.stage.Stage;
+
 public class Application {
     public static void main(String[] args) {
         ToDoScene tds = new ToDoScene();
+        Stage stage = new Stage();
         try {
-            tds.start();
+            tds.start(stage);
         } catch (Exception e) {
             e.printStackTrace();
         }
